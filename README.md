99ba6b8f7ea9c439ec52b841d36e36328949c297
André Yoshida <andreyoshidamct@hotmail.com>
2021-02-18 22:05:22 -0300

    pushing project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Smartphone_activity_identification
 Project developed in the post graduate subject IA048-Machine Learning at FEEC,UNICAMP. Multi-class classification using logistic regression(softmax) and k-nearest neighbors
+
+# Logistic Regression
+For logistic regression problem, it was used the holdout cross-validation method, and for the model it was used a baseline method(no grid-search for hyperparameters) just to compare with KNN algorithm.
+
+# KNN
+For KNN problem, the number of neighbors was determined by using a 5-fold cross-validation method, and by analyzing the F1-score metric, the number found was k = 16.
+
+### F1-score macro-average
+As the problem consists of a 5 classes prediction, global accuracy F1-score macro-average was used trying to avoid the misleading information that the umbalanced dataset could lead us.
+
+# Results:
+By comparing the F1-score accuracy and the runtime, the Logistic Regression outperforms the KNN algorithm in both metrics.
---
+++ sources/knn.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+from pandas.plotting import scatter_matrix
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+
+def compute_macro_avg(y_true,y_predicted):
+    
+    '''
+    Reference: https://datascience.stackexchange.com/questions/15989/micro-average-vs-macro-average-performance-in-a-multiclass-classification-settin
+
+    Macro avg consiste em avaliar, para um conjunto de classes bem balanceados, i.e, se há 500 classes 1, valores devem
+    ser proximos para outras classes também
+    
+    Para precisão, por exemplo:
+    
+    PR = SOMAR precisão de cada classe e dividir pelo numero de classes
+    
+    O mesmo para recall,
+    
+    F1_macro = 2*(Pmacro+Rmacro)/(Pmacro+Rmacro)
+    
+    Parametros de Entrada da função compute_macro_avg():
+    
+    Algorithm: algorithm class to evaluate
+    
+    y_true
+    
+    y_predicted
+    
+    
+    
+    Parametros de saída:
+    
+    Precision,recall,f1
+    
+   
+    
+    '''
+    # cria matriz de confusao para as l classes
+    matrix = confusion_matrix(y_true, y_predicted)
+    
+    # obtemos quantidade de classes a serem avaliadas
+    l,_ = matrix.shape
+    
+    precision_v = []
+    recall_v = []
+    f1_v = []
+    for class_ in range(l): # < matrix rows and columns starts at 0 index
+        TP = matrix[class_,class_]
+        FP = matrix[:,class_].sum() - TP
+        FN = matrix[class_,:].sum() - TP
+        
+        precision = TP/(TP+FP)
+        recall = TP/(TP+FN)
+        f1 = 2*(precision*recall)/(precision+recall)
+        precision_v.append(precision)
+        recall_v.append(recall)
+        f1_v.append(f1)
+    
+    return sum(precision_v)/l,sum(recall_v)/l,sum(f1_v)/l
+
+
+if __name__ == '__main__':
+
+
+    X_test = pd.read_csv('../data/X_test.txt',delim_whitespace=True, header=None)
+    X_train = pd.read_csv('../data/X_train.txt',delim_whitespace=True, header=None)
+    y_test = pd.read_csv('../data/y_test.txt',delim_whitespace=True, header=None)
+    y_train = pd.read_csv('../data/y_train.txt',delim_whitespace=True, header=None)
+
+    #==================================================================================
+    # using k-fold as cross-validation method 
+    k_nb = np.arange(1,21)
+    kf = KFold(5)
+    test_f1_metric = []
+    for n_vizinho in k_nb:
+        f1_v = []
+        for k,(train_index, test_index) in enumerate(kf.split(X_train,y_train)):
+            print('k_fold: {0}'.format(k))
+            neigh = KNeighborsClassifier(n_neighbors= n_vizinho)# < mudar metrica
+            neigh.fit(X_train.iloc[train_index], y_train.iloc[train_index][0])
+            y_knn_predict = neigh.predict(X_train.iloc[test_index])
+            _,_,f1 = compute_macro_avg(y_train.iloc[test_index][0],y_knn_predict)
+            f1_v.append(f1)
+            print(f1)
+        test_f1_metric.append(sum(f1_v)/5)
+
+
+    ##===================================================================================
+    # the best f1-metric value obtained in the last step was for k = 16, also shown in image
+    neigh = KNeighborsClassifier(n_neighbors= 16)# < mudar metrica
+    neigh.fit(X_train, y_train[0])
+    y_knn_predict = neigh.predict(X_test)
+    _,_,f1 = compute_macro_avg(y_test,y_knn_predict)
